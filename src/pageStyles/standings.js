94523cb257ba5a429f36ee80710b4ab94e9e8174
Tyler C <ctylertim@gmail.com>
2022-01-29 16:31:28 -0500

    ui mistake, fixed it

--- src/pageStyles/standings.js
+++ src/pageStyles/standings.js
@@ -64,7 +64,6 @@ export const ITC = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-    outline: 1px solid red;
 `;
 
 export const SDCIconContainer = styled.div`
